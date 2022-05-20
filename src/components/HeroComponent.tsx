dd46248d7a5f29537aff4cfbc57614a951754add
Thomas <65050898+tvonment@users.noreply.github.com>
2022-05-20 15:20:37 +0000

    add cards

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,8 +1,17 @@
+import { Button, Card } from "react-bootstrap";
 import Hero from "../models/hero";
 import classes from "./HeroComponent.module.scss";
 
 
 const HeroComponent: React.FC<{ hero: Hero; onDeleteHero: () => void }> = (props) => {
-    return (<li className={classes.hero} key={props.hero.id} onClick={props.onDeleteHero}>{props.hero.name} - {props.hero.power}</li>);
+    return (
+        <Card className={classes.hero} key={props.hero.id}>
+            <Card.Body>
+                <Card.Title>{props.hero.name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{props.hero.power}</Card.Subtitle>
+                <Button onClick={props.onDeleteHero} variant="danger">Delete</Button>
+            </Card.Body>
+        </Card>
+    );
 }
 export default HeroComponent;
--- src/components/HeroesComponent.tsx
+++ src/components/HeroesComponent.tsx
@@ -2,16 +2,21 @@ import React, { useContext } from "react";
 import HeroComponent from "./HeroComponent";
 import { HeroesContext } from "../store/heroes-context";
 import classes from "./HeroesComponent.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
 
 const HeroesComponent: React.FC = () => {
     const heroesCtx = useContext(HeroesContext)
 
     return (
-        <ul className={classes.heroes}>
-            {heroesCtx.heroes.map((hero) => (
-                <HeroComponent key={hero.id} hero={hero} onDeleteHero={heroesCtx.deleteHero.bind(null, hero)} />
-            ))}
-        </ul>
+        <Container className={classes.heroes}>
+            <Row>
+                {heroesCtx.heroes.map((hero) => (
+                    <Col sm={3}>
+                        <HeroComponent key={hero.id} hero={hero} onDeleteHero={heroesCtx.deleteHero.bind(null, hero)} />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 };
 export default HeroesComponent;
